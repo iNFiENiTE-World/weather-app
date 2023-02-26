6be9262dabf677136a9d13e2062b6790dac66f7e
iNFiENiTE-World <infienite@gmail.com>
2023-02-26 08:17:38 +0800

    Logging the properties of the response

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const request = require('request')
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=${process.env.QUERY}`
+const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=${process.env.QUERY}&units=s`
 
-request({ url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url, json: true }, (error, response) => {
+    const { current } = response.body
+    console.log(`It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`);
 })
